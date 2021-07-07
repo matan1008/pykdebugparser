e4231f5c13b9c269f25d4cd1c37cad0133ddae67
matan <matan1008@gmail.com>
2021-07-07 09:17:24 +0300

    Trace: Move handlers to a different file, add some handlers.

---
+++ pykdebugparser/trace_handlers/trace.py
@@ -0,0 +1,112 @@
+from dataclasses import dataclass
+from typing import List
+
+from pykdebugparser.kd_buf_parser import ProcessData
+from pykdebugparser.kevent import DgbFuncQual
+
+
+@dataclass
+class TraceDataNewthread:
+    ktraces: List
+    tid: int
+    pid: int
+    is_exec_copy: int
+    uniqueid: int
+
+    def __str__(self):
+        return f'New thread {self.tid} of parent: {self.pid}'
+
+
+@dataclass
+class TraceDataExec:
+    ktraces: List
+    pid: int
+    fsid: int
+    fileid: int
+
+    def __str__(self):
+        return f'New process pid: {self.pid}'
+
+
+@dataclass
+class TraceStringGlobal:
+    ktraces: List
+    debugid: int
+    str_id: int
+    vstr: str
+
+    def __str__(self):
+        return f'New global string: "{self.vstr}", id: {self.str_id}'
+
+
+@dataclass
+class TraceStringNewthread:
+    ktraces: List
+    name: List
+
+    def __str__(self):
+        return f'New thread of parent: {self.name}'
+
+
+@dataclass
+class TraceStringExec:
+    ktraces: List
+    name: List
+
+    def __str__(self):
+        return f'New process name: {self.name}'
+
+
+def handle_trace_data_newthread(parser, events):
+    result = events[0].values
+    parser.last_data_newthread = TraceDataNewthread(events, result[0], result[1], result[2], result[3])
+    return parser.last_data_newthread
+
+
+def handle_trace_data_exec(parser, events):
+    result = events[0].values
+    parser.last_data_exec = TraceDataExec(events, result[0], result[1], result[2])
+    return parser.last_data_exec
+
+
+def handle_trace_string_global(parser, events):
+    debugid = 0
+    str_id = 0
+    vstr = b''
+    lookup_events = []
+    for event in events:
+        lookup_events.append(event)
+        if event.func_qualifier & DgbFuncQual.DBG_FUNC_START.value:
+            debugid = event.values[0]
+            str_id = event.values[1]
+            vstr += event.data[16:]
+        else:
+            vstr += event.data
+
+        if event.func_qualifier & DgbFuncQual.DBG_FUNC_END.value:
+            break
+    event = TraceStringGlobal(lookup_events, debugid, str_id, vstr.replace(b'\x00', b'').decode())
+    if event.vstr:
+        parser.global_strings[event.str_id] = event.vstr
+    return event
+
+
+def handle_trace_string_newthread(self, events):
+    event = TraceStringNewthread(events, events[0].data.replace(b'\x00', b'').decode())
+    self.thread_map[self.last_data_newthread.tid] = ProcessData(self.last_data_newthread.pid, event.name)
+    return event
+
+
+def handle_trace_string_exec(self, events):
+    event = TraceStringExec(events, events[0].data.replace(b'\x00', b'').decode())
+    self.thread_map[events[0].tid] = ProcessData(self.last_data_exec.pid, event.name)
+    return event
+
+
+handlers = {
+    'TRACE_DATA_NEWTHREAD': handle_trace_data_newthread,
+    'TRACE_DATA_EXEC': handle_trace_data_exec,
+    'TRACE_STRING_GLOBAL': handle_trace_string_global,
+    'TRACE_STRING_NEWTHREAD': handle_trace_string_newthread,
+    'TRACE_STRING_EXEC': handle_trace_string_exec,
+}
--- pykdebugparser/traces_parser.py
+++ pykdebugparser/traces_parser.py
@@ -1,74 +1,27 @@
 from collections import namedtuple
-from dataclasses import dataclass
-from typing import List
 
 from pykdebugparser.kevent import DgbFuncQual
-from pykdebugparser.kd_buf_parser import ProcessData
 from pykdebugparser.trace_handlers.bsd import handlers as bsd_handlers
 from pykdebugparser.trace_handlers.fsystem import handlers as fsystem_handlers
 from pykdebugparser.trace_handlers.mach import handlers as mach_handlers
 from pykdebugparser.trace_handlers.perf import handlers as perf_handlers
+from pykdebugparser.trace_handlers.trace import handlers as trace_handlers
 
 Vnode = namedtuple('Vnode', ['ktraces', 'vnode_id', 'path'])
-
-
-@dataclass
-class TraceDataNewthread:
-    ktraces: List
-    tid: int
-    pid: int
-    is_exec_copy: int
-    uniqueid: int
-
-    def __str__(self):
-        return f'New thread {self.tid} of parent: {self.pid}'
-
-
-@dataclass
-class TraceStringNewthread:
-    ktraces: List
-    name: List
-
-    def __str__(self):
-        return f'New thread of parent: {self.name}'
-
-
-@dataclass
-class TraceDataExec:
-    ktraces: List
-    pid: int
-    fsid: int
-    fileid: int
-
-    def __str__(self):
-        return f'New process pid: {self.pid}'
-
-
-@dataclass
-class TraceStringExec:
-    ktraces: List
-    name: List
-
-    def __str__(self):
-        return f'New process name: {self.name}'
 
 
 class TracesParser:
     def __init__(self, trace_codes_map, thread_map):
         self.trace_codes = trace_codes_map
         self.on_going_events = {}
+        self.on_going_traces = {}
+        self.global_strings = {}
         self.thread_map = thread_map
         self.qualifiers_actions = {
             DgbFuncQual.DBG_FUNC_START.value: self._feed_start_event,
             DgbFuncQual.DBG_FUNC_END.value: self._feed_end_event,
             DgbFuncQual.DBG_FUNC_ALL.value: self._feed_single_event,
             DgbFuncQual.DBG_FUNC_NONE.value: self._feed_single_event,
-        }
-        self.trace_handlers = {
-            'TRACE_DATA_NEWTHREAD': self.handle_trace_data_newthread,
-            'TRACE_DATA_EXEC': self.handle_trace_data_exec,
-            'TRACE_STRING_NEWTHREAD': self.handle_trace_string_newthread,
-            'TRACE_STRING_EXEC': self.handle_trace_string_exec,
         }
         self.last_data_newthread = None
         self.last_data_exec = None
@@ -77,15 +30,15 @@ class TracesParser:
         self.handlers.update(fsystem_handlers)
         self.handlers.update(mach_handlers)
         self.handlers.update(perf_handlers)
+        self.handlers.update(trace_handlers)
 
     def feed(self, event):
         if event.eventid in self.trace_codes:
             trace_name = self.trace_codes[event.eventid]
-            if trace_name in self.trace_handlers:
-                self.trace_handlers[trace_name]([event])
-                return
+            if trace_name in trace_handlers:
+                return self.qualifiers_actions[event.func_qualifier](event, self.on_going_traces)
 
-        return self.qualifiers_actions[event.func_qualifier](event)
+        return self.qualifiers_actions[event.func_qualifier](event, self.on_going_events)
 
     def feed_generator(self, generator):
         for event in generator:
@@ -129,45 +82,25 @@ class TracesParser:
     def parse_vnodes(self, events):
         return list(self.vnode_generator([e for e in events if self.trace_codes.get(e.eventid) == 'VFS_LOOKUP']))
 
-    def handle_trace_data_newthread(self, events):
-        result = events[0].values
-        self.last_data_newthread = TraceDataNewthread(events, result[0], result[1], result[2], result[3])
-        return self.last_data_newthread
-
-    def handle_trace_string_newthread(self, events):
-        event = TraceStringNewthread(events, events[0].data.replace(b'\x00', b'').decode())
-        self.thread_map[self.last_data_newthread.tid] = ProcessData(self.last_data_newthread.pid, event.name)
-        return event
-
-    def handle_trace_data_exec(self, events):
-        result = events[0].values
-        self.last_data_exec = TraceDataExec(events, result[0], result[1], result[2])
-        return self.last_data_exec
-
-    def handle_trace_string_exec(self, events):
-        event = TraceStringExec(events, events[0].data.replace(b'\x00', b'').decode())
-        self.thread_map[events[0].tid] = ProcessData(self.last_data_exec.pid, event.name)
-        return event
-
-    def _feed_start_event(self, event):
-        if event.tid not in self.on_going_events:
-            self.on_going_events[event.tid] = [event]
+    def _feed_start_event(self, event, state):
+        if event.tid not in state:
+            state[event.tid] = [event]
         else:
-            self.on_going_events[event.tid].append(event)
+            state[event.tid].append(event)
 
-    def _feed_end_event(self, event):
-        if event.tid not in self.on_going_events:
+    def _feed_end_event(self, event, state):
+        if event.tid not in state:
             # Event end without start.
             return
-        if event.eventid != self.on_going_events[event.tid][0].eventid:
-            self.on_going_events[event.tid].append(event)
+        if event.eventid != state[event.tid][0].eventid:
+            state[event.tid].append(event)
             return
-        events = self.on_going_events.pop(event.tid)
+        events = state.pop(event.tid)
         events.append(event)
         return self.parse_event_list(events)
 
-    def _feed_single_event(self, event):
-        if event.tid in self.on_going_events:
-            self.on_going_events[event.tid].append(event)
+    def _feed_single_event(self, event, state):
+        if event.tid in state:
+            state[event.tid].append(event)
         else:
             return self.parse_event_list([event])
